bb3ee7e44243344d609b1e7164f61cdb1e7f8841
Maya Stucky <mayaatstucky@gmail.com>
2020-06-13 14:46:21 -0400

    got items  to save to local storage and save on refresh

--- script.js
+++ script.js
@@ -23,4 +23,39 @@ $(document).ready(function () {
     }
   });
 
+  //save to local storage
+  var nine = $("#9");
+  var ten = $("#10");
+  var eleven = $("#11");
+  var twelve = $("#12");
+  var one = $("#13");
+  var two = $("#14");
+  var three = $("#15");
+  var four = $("#16");
+  var five = $("#17");
+
+  //saves to local storage during a click on each of the buttons
+  //key: value
+  $("button").on("click", function () {
+    localStorage.setItem("9", nine.val());
+    localStorage.setItem("10", ten.val());
+    localStorage.setItem("11", eleven.val());
+    localStorage.setItem("12", twelve.val());
+    localStorage.setItem("1", one.val());
+    localStorage.setItem("2", two.val());
+    localStorage.setItem("3", three.val());
+    localStorage.setItem("4", four.val());
+    localStorage.setItem("5", five.val());
+  });
+
+  //now we need to retrieve that information so it's saved on refresh
+
+  $("#9").append(localStorage.getItem("9"));
+  $("#10").append(localStorage.getItem("10"));
+  $("#11").append(localStorage.getItem("12"));
+  $("#13").append(localStorage.getItem("1"));
+  $("#14").append(localStorage.getItem("2"));
+  $("#15").append(localStorage.getItem("3"));
+  $("#16").append(localStorage.getItem("4"));
+  $("#17").append(localStorage.getItem("5"));
 });
